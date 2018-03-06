b572b5844ea9ba37638bd9d27341977e74593fb5
Gierczak4 <>
2018-03-06 10:58:33 +0100

    pierwszy commit

---
+++ Blog1018/src/blog1018/Blog1018.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blog1018;
+
+/**
+ *
+ * @author Student
+ */
+public class Blog1018 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("halooo");
+    }
+    
+}
